9ed10c435cfddab5cee56e1a5b36f4e8da8f31bc
Jay Jagpal <jay@strangemother.com>
2018-03-03 03:14:25 +0000

    Settings load from config

--- player/player.py
+++ player/player.py
@@ -1,13 +1,15 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QSizeGrip
 from media import VideoFrame
+from flags import FlagsMixin, FRAMELESS
 
 
-class MediaPlayer(QWidget):
+class MediaPlayer(QWidget, FlagsMixin):
     '''A application interface hosting a VideoFrame and managing its
     interactivity.'''
 
     frameless = False
+    flags = FRAMELESS
 
     def __init__(self, settings=None):
         super().__init__()
@@ -15,14 +17,35 @@ class MediaPlayer(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle('Video Player')
 
-        # no windows buttons or edge
-        if self.settings.get('frameless', self.frameless):
-            self.setWindowFlags(Qt.FramelessWindowHint)
-            self.setFocusPolicy(Qt.WheelFocus)
-        self.resize(500, 400)
+        self.setFocusPolicy(Qt.WheelFocus)
+
+        self.config_set_title()
+        self.config_set_size()
+        self.set_flags()
+
         self.build_view()
+
+    def config_set_frame(self):
+        # no windows buttons or edge
+        on = self.settings.get('frameless', self.frameless)
+        flag = Qt.FramelessWindowHint
+
+        if on:
+            self.add_flag(flag)
+            return True
+            #self.setWindowFlags(Qt.FramelessWindowHint)
+
+        self.remove_flag(flag)
+        return False
+
+    def config_set_title(self):
+        title = self.settings.get('player_title', 'Video Player')
+        self.setWindowTitle(title)
+
+    def config_set_size(self):
+        size = self.settings.get('init_size', (500, 400,))
+        self.resize(*size)
 
     def build_view(self):
         # self.setStyleSheet("background-color: #3f4d82")
@@ -59,9 +82,9 @@ class ControlPanel(QWidget):
         # self.resize(50, 200)
         self.setMaximumHeight(60)
         self.setMinimumHeight(20)
-        self.setMinimumWidth(500)
+        self.setMinimumWidth(20)
         self.label = QLabel("controls", self)
         self.label2 = QLabel("other controls", self)
-        self.label2.setGeometry(40, 50, 20,20)
+        self.label2.setGeometry(0, 50, 20,20)
         # self.show()
 
